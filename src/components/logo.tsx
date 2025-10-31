726404e740727eafb10efee3dc5c5ad591d498eb
ALAMIN AMINU <aminualamin286@gmail.com>
2025-10-31 09:07:37 +0000

    Remove the logo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,16 +1,9 @@
 import Link from "next/link";
 import { cn } from "@/lib/utils";
-import Image from "next/image";
 
 export const Logo = ({ className }: { className?: string }) => {
   return (
     <Link href="/" className={cn("flex items-center gap-2 text-lg font-bold font-headline text-primary", className)}>
-      <Image
-        src="https://firebasestudio.app/assets/project-images/432bd31d-b30c-4330-9a4f-f064f2038b36.png"
-        alt="Kano State Government Logo"
-        width={40}
-        height={40}
-      />
       <span>KELEX</span>
     </Link>
   );
