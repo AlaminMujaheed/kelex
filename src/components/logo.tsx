f0c2d8b581457803942e91cceb2f842e19890725
ALAMIN AMINU <aminualamin286@gmail.com>
2025-11-04 14:10:37 +0000

    Build Error
    Parsing ecmascript source code failed
    
    ./src/components/lo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,20 +1,23 @@
 import Image from "next/image";
+import Link from "next/link";
 import { cn } from "@/lib/utils";
 import { PlaceHolderImages } from "@/lib/placeholder-images";
 
 export const Logo = ({ className }: { className?: string }) => {
   const logoImage = PlaceHolderImages.find(img => img.id === "logo-1");
   return (
-    <Link href="/" className={cn("flex items-center gap-2 text-lg font-bold font-headline text-primary", className)}>
+    <div className={cn("flex items-center gap-2 text-lg font-bold font-headline text-primary", className)}>
       {logoImage && (
-        <Image
-          src={logoImage.imageUrl}
-          alt={logoImage.description || "Logo"}
-          width={36}
-          height={36}
-          data-ai-hint={logoImage.imageHint}
-          priority
-        />
+        <Link href="/">
+            <Image
+              src={logoImage.imageUrl}
+              alt={logoImage.description || "Logo"}
+              width={36}
+              height={36}
+              data-ai-hint={logoImage.imageHint}
+              priority
+            />
+        </Link>
       )}
       <span>KELEX</span>
     </div>
